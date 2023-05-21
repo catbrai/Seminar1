8cc94159acfb5edd8e5a6d4a79fdc83da1e9d806
Anastasia Ivlyushkina <ivlu@inbox.ru>
2023-05-21 19:37:45 +0300

    Task03 formating

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,26 +1,62 @@
-﻿Console.WriteLine("Введите число, соответствующее дню недели: ");
-int number = Convert.ToInt32(Console.ReadLine());
+﻿// Console.WriteLine("Введите число, соответствующее дню недели: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-if (number == 1)
+// if (number == 1)
+//     Console.WriteLine("Понедельник");
+
+// else  if (number == 2)
+//     Console.WriteLine("Вторник");
+
+// else if (number == 3)
+//     Console.WriteLine("Среда");
+
+// else  if (number == 4)
+//     Console.WriteLine("Четверг");
+
+// else  if (number == 5)
+//     Console.WriteLine("Пятница");
+
+// else  if (number == 6)
+//     Console.WriteLine("Суббота");
+
+// else  if (number == 7)
+//     Console.WriteLine("Воскресенье");
+
+// else
+//     Console.WriteLine("Некорректный ввод");
+
+    Console.WriteLine("Введите число, соответствующее дню недели: ");
+string number = Console.ReadLine();
+
+if (number == "1")
+{
     Console.WriteLine("Понедельник");
-
-else  if (number == 2)
+}
+else if (number == "2")
+{
     Console.WriteLine("Вторник");
-
-else if (number == 3)
+}
+else if (number == "3")
+{
     Console.WriteLine("Среда");
-
-else  if (number == 4)
+}
+else if (number == "4")
+{
     Console.WriteLine("Четверг");
-
-else  if (number == 5)
+}
+else if (number == "5")
+{
     Console.WriteLine("Пятница");
-
-else  if (number == 6)
+}
+else if (number == "6")
+{
     Console.WriteLine("Суббота");
-
-else  if (number == 7)
+}
+else if (number == "7")
+{
     Console.WriteLine("Воскресенье");
-    
+}
 else
+{
     Console.WriteLine("Некорректный ввод");
+}
