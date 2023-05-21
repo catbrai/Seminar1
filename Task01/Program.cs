6e712b1696dab3cb5a6a2fef50657e990819c8f6
Anastasia Ivlyushkina <ivlu@inbox.ru>
2023-05-21 16:15:45 +0300

    Task001

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите целое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целое число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine($"Да, {numberA} является квадратом числа {numberB}");
+}
+else 
+{
+    Console.WriteLine($"Нет, {numberA} не является квадратом числа {numberB}");
+};
+
