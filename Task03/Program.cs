a4f479c8c3c40c13b80cb5cb63ad18cc1fadcd88
Anastasia Ivlyushkina <ivlu@inbox.ru>
2023-05-21 16:43:37 +0300

    formating task03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,26 +1,25 @@
 ﻿Console.WriteLine("Введите число, соответствующее дню недели: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-   if(number == 1)
+if (number == 1)
     Console.WriteLine("Понедельник");
 else
-    if(number == 2)
+ if (number == 2)
     Console.WriteLine("Вторник");
 else
-    if(number == 3)
+ if (number == 3)
     Console.WriteLine("Среда");
 else
-    if(number == 4)
+ if (number == 4)
     Console.WriteLine("Четверг");
 else
-    if(number == 5)
+ if (number == 5)
     Console.WriteLine("Пятница");
 else
-    if(number == 6)
+ if (number == 6)
     Console.WriteLine("Суббота");
 else
-    if(number == 7)
+ if (number == 7)
     Console.WriteLine("Воскресенье");
-else 
-
+else
     Console.WriteLine("Неккоректный ввод");
