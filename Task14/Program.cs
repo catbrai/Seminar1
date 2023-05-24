9b6ce4b0d26d2c1646a6b9ca68cf23cbc0554fe1
Anastasia Ivlyushkina <ivlu@inbox.ru>
2023-05-25 00:55:46 +0300

    finished Task14,16

---
+++ Task14/Program.cs
@@ -0,0 +1,19 @@
+﻿// 14. Напишите программу,
+//  1. которая принимает на вход число 
+//  2.и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+bool DevidedByTwoNumbers( int num, int num1, int num2)// devided - разделенный
+{
+    return num % num1 == 0 && num % num2 == 0;
+// if((num % num1 == 0) && (num % num2 == 0)) return true;
+// else return false;
+} 
+
+Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+bool myresult = DevidedByTwoNumbers(number, 7, 23);
+Console. WriteLine(myresult ?"да": "нет");
+
---
+++ Task16/Program.cs
@@ -0,0 +1,20 @@
+﻿// 16. Напишите программу, которая 
+// 1.принимает на вход два числа 
+// 2 и проверяет, является ли одно число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+bool IsSquareTwoNumbers(int num1, int num2)
+{
+    return num1 == num2 * num2 || num2 == num1 * num1;// || - логический оператор ИЛИ, && - И, одновременно
+}
+
+Console.Write("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsSquareTwoNumbers(number1, number2);
+Console.WriteLine(result ? "да" : "нет");
