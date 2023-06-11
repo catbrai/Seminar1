24d09e7c460c36f1164a2912d8e4dbc0c6926533
Anastasia Ivlyushkina <ivlu@inbox.ru>
2023-06-11 09:13:32 +0300

    task39,40,42,44,45

---
+++ Task40/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 40: Напишите программу, которая 
+// принимает на вход три числа и 
+// проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+int Start()
+{
+    Console.Write("Введите целоe положительное число, соответствующее длине стороны треугольника: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool Treangle(int numA, int numB, int numC)
+{
+    return ((numA < numB + numC) && (numB < numA + numC) && (numC < numA + numB));
+    }
+
+int numberA = Start();
+int numberB = Start();
+int numberC = Start();
+if (numberA <= 0 || numberB <= 0 || numberC <= 0)
+{
+    Console.WriteLine("Вы ввели некорректное число");
+    return;
+}
+
+Console.WriteLine(Treangle (numberA, numberB, numberC)
+                ? $" Да, треугольник со сторонами такой длины может существовать."
+                : $" Нет, треугольник со сторонами такой длины не может существовать.");
---
+++ Task44/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+
+int [] arrayFib = Fibonacci(8);
+PrintArray(arrayFib);
+
+int[] Fibonacci(int size)
+{
+    int[] array = new int[size];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+
+
+void PrintArray(int[]arr, string sep = ",")
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+
